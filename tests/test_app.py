fcdf6bbaf8b3cb91b9a1b888d46742ad8ac20129
Kate Whalen <whalenka88@gmail.com>
2021-04-05 20:24:30 +0100

    Add test directory and test for fibonacci generator

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app
+
+
+class TestApp(unittest.TestCase):
+
+    def test_fibonacci(self):
+        reader = app.fibonacci()
+        self.assertEqual(next(reader), 0), "Should be 0"
+        self.assertEqual(next(reader), 1), "Should be 1"
+        self.assertEqual(next(reader), 1), "Should be 1"
+        self.assertEqual(next(reader), 2), "Should be 2"
+        self.assertEqual(next(reader), 3), "Should be 3"
+        self.assertEqual(next(reader), 5), "Should be 5"
+
+
+if __name__ == '__main__':
+    unittest.main()
